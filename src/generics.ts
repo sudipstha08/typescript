0b9ceafcde1d721b1ed8972bfaea2d05bb26a29f
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-23 22:16:11 +0545

    add: generics

---
+++ src/generics.ts
@@ -0,0 +1,9 @@
+//  The identity function is a function that will return back whatever is passed in. 
+
+function identity(arg: number): number {
+  return arg;
+}
+
+
+const a = identity(5)
+console.log("a", a)
